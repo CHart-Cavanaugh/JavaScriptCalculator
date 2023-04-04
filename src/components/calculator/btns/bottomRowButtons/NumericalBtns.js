3a897c42336d581f95dd2a234753e3d699cff836
Christopher Hart-Cavanaugh <christopher.hartcava@gmail.com>
2023-04-04 12:49:02 -0700

    Cleaned up the code for NumericalBtns.js

--- src/components/calculator/btns/bottomRowButtons/NumericalBtns.js
+++ src/components/calculator/btns/bottomRowButtons/NumericalBtns.js
@@ -10,7 +10,9 @@ const NumericalBtns = (props) => {
 
   const displayText = useSelector(state => state.displayText);
   const dispatch = useDispatch();
+
   const numberNames = [
+
     "zero",
     "one",
     "two",
@@ -20,7 +22,8 @@ const NumericalBtns = (props) => {
     "six",
     "seven",
     "eight",
-    "nine",
+    "nine"
+
   ];
   const handleClick = (index) => {
 
@@ -35,16 +38,20 @@ const NumericalBtns = (props) => {
       };
       const lastCharIsNum = () => {
 
-        return (
-          displayInput[displayInput.length - 1] === "." ||
-          displayInput[displayInput.length - 1].match(/[0-9]/) !== null
-        );
+        return displayInput[displayInput.length - 1].match(/[0-9]/) !== null;
+
+      };
+      const lastCharIsDecimal = () => {
+
+        return displayInput[displayInput.length - 1] === ".";
 
       };
       const lastNumIsZero = () => {
 
         const inputNums = displayInput.match(/[0-9]+[.]?[0-9]*/g);
         const lastNum = inputNums[inputNums.length - 1];
+
+
 
         return lastNum === "0";
 
@@ -54,24 +61,25 @@ const NumericalBtns = (props) => {
         return displayInput[displayInput.length - 1].match(/[/*+-]/) !== null;
 
       };
-
       const updateDisplay = () => {
 
-        if (lastCharIsEquals() || displayText.result === "0" && displayInput === "") {
+        if (lastCharIsEquals() || (displayText.result === "0" && displayInput === "")) {
+
           dispatch(setInput(`${index}`));
           dispatch(setResult(`${index}`));
+
         }
-        else if (lastCharIsNum()) {
+        else if (lastCharIsNum() || lastCharIsDecimal()) {
 
           if (lastNumIsZero()) {
 
-            dispatch(setInput(displayInput.slice(0, displayInput.length - 1) + index));
-            dispatch(setResult(`${displayText.result.slice(0, displayInput.length - 1)}${index}`));
+            dispatch(setInput(`${displayInput.slice(0, displayInput.length - 1)}${index}`));
+            dispatch(setResult(`${index}`));
 
           }
           else {
 
-            dispatch(setInput(displayInput + index));
+            dispatch(setInput(`${displayInput}${index}`));
             dispatch(setResult(`${displayText.result}${index}`));
 
           }
@@ -79,8 +87,8 @@ const NumericalBtns = (props) => {
         }
         else if (lastCharIsOp()) {
 
-          dispatch(setInput(displayInput + " " + index));
-          dispatch(setResult(index));
+          dispatch(setInput(`${displayInput} ${index}`));
+          dispatch(setResult(`${index}`));
 
         }
 
